fb54dca16ec18f3a7778d3e9ba6bc41a16ed189d
Ibtisam Hemmo <ibtisamhemmo@gmail.com>
2021-04-10 23:13:05 +0300

    commit

---
+++ jsFiles/index.js
@@ -0,0 +1,7 @@
+// alert("hi");
+// const menuToggle = document.querySelector('.toggle');
+// const showcaseToggle = document.querySelector('.showcase');
+
+// menuToggle.addEventListener('click', ()=> menuToggle.classList.toggle("active"));
+// showcaseToggle.addEventListener('click', ()=> showcaseToggle.classList.toggle("active"));
+
